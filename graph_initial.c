da9143b2dc27b7ea1b72a8d3a834bff5f9f014da
Anssoumane Sissokho <152038588+anssoumane14@users.noreply.github.com>
2025-02-27 10:02:41 +0100

    Add files via upload

--- graph_initial.c
+++ graph_initial.c
@@ -3,8 +3,11 @@
 #define NB_SOMMETS 9
 #define INFINI 99999
 
-// Indices : A=0, B=1, C=2, D=3, E=4, F=5, G=6, H=7, I=8
-int matrice[NB_SOMMETS][NB_SOMMETS] = {
+// Tableau contenant les noms des sommets (A, B, C, ... I)
+char sommets[NB_SOMMETS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+
+// Matrice des distances entre les sommets
+int distances[NB_SOMMETS][NB_SOMMETS] = {
     //    A      B      C      D      E      F      G      H      I
     {    0,     6,     4,     3,  INFINI, INFINI, INFINI, INFINI, INFINI }, // A
     {INFINI,    0,  INFINI, INFINI,    3,     3,  INFINI, INFINI, INFINI }, // B
@@ -17,19 +20,38 @@ int matrice[NB_SOMMETS][NB_SOMMETS] = {
     {INFINI,INFINI,INFINI, INFINI,INFINI, INFINI, INFINI, INFINI,    0   }  // I
 };
 
-int main() 
-{
-    // Affichage pour vérifier
-    printf("Matrice d’adjacence (A..I) :\n");
-    for(int i = 0; i < NB_SOMMETS; i++){
-        for(int j = 0; j < NB_SOMMETS; j++){
-            if(matrice[i][j] == INFINI) {
-                printf("INF\t");
+// Fonction pour afficher les sommets
+void afficher_sommets() {
+    printf("Sommets : ");
+    for (int i = 0; i < NB_SOMMETS; i++) {
+        printf("%c ", sommets[i]);
+    }
+    printf("\n");
+}
+
+// Fonction pour afficher la matrice des distances
+void afficher_distances() {
+    printf("\nMatrice des distances :\n    ");
+    for (int i = 0; i < NB_SOMMETS; i++) {
+        printf("%c\t", sommets[i]); // Affichage des noms des colonnes
+    }
+    printf("\n");
+
+    for (int i = 0; i < NB_SOMMETS; i++) {
+        printf("%c | ", sommets[i]); // Affichage des noms des lignes
+        for (int j = 0; j < NB_SOMMETS; j++) {
+            if (distances[i][j] == INFINI) {
+                printf("INF\t");  // Affichage des valeurs infinies
             } else {
-                printf("%d\t", matrice[i][j]);
+                printf("%d\t", distances[i][j]);
             }
         }
         printf("\n");
     }
+}
+
+int main() {
+    afficher_sommets();
+    afficher_distances();
     return 0;
 }
